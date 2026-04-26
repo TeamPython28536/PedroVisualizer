fcfbe3dd69e0b1f6d6f7eb9845aab5d9f9ce99ac
Ben McAvoy <ben.mcavoy@tutanota.com>
2026-04-26 23:49:36 +0100

    feat: social embed

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         "decode.webp",
         "robot.png",
         "apple-touch-icon.png",
+        "social-card.png",
       ],
       manifest: {
         name: "Pedro Path Visualizer",
